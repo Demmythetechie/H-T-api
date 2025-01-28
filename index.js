53cb76cef496fe766febe5322f8f6a6b99c001ee
Demmythetechie <demmythetechie@gmail.com>
2025-01-29 00:10:53 +0100

    order of code updated, Nothing was added

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.listen(3000, () => {
-    console.log(`App listening at http://localhost:${3000}`);
-});
-
-
 app.post('/signup', (req, res) => {
     const message = req.body;
     res.send(`Gotten the data needed ${message}`);
+});
+
+app.listen(3000, () => {
+    console.log(`App listening at http://localhost:${3000}`);
 });
