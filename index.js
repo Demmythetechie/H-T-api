70c48296cc249b875b5fd822d7ab3e2651ef6dbe
Demmythetechie <demmythetechie@gmail.com>
2025-01-29 13:06:46 +0100

    updated the message variable from req.body to req again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signup', (req, res) => {
-    const message = req.body;
+    const message = req;
     res.send(`Gotten the data needed ${message}`);
 });
 
