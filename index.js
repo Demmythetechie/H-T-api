bbe3fb4eae5ab7ee51999c2bfe86ff04b1f67cc4
Demmythetechie <demmythetechie@gmail.com>
2025-02-03 23:02:22 +0100

    still on validating if the mongoose connection code works fine

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const rules = cors();
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(e.json());
 
+var test = '';
+
 (async function () {
     try {
         await mongoose.connect('mongodb+srv://demmythetechie:Alade3015@h-t.8p6yq.mongodb.net/?retryWrites=true&w=majority&appName=H-T')
-        console.log('connected Successfully ✅')
+        console.log('connected Successfully ✅');
+        test = 'connected Successfully ✅'
     } catch(error) {
         console.error("MongoDB Connection Error ❌:", error);
+        test = 'MongoDB Connection Error ❌:';
         process.exit(1);
     }
 })();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send(test);
+
 });
 
 app.post('/signup', (req, res) => {
