01a3507b53b565d698a1ac8c2503c3dfce904b0c
Demmythetechie <demmythetechie@gmail.com>
2025-02-03 20:25:39 +0100

    imported mongoose and connected it to my atlas cluster

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import e from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 const app = e();
 const rules = cors();
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(e.json());
+
+mongoose.connect('mongodb+srv://demmythetechie:Alade3015@h-t.8p6yq.mongodb.net/?retryWrites=true&w=majority&appName=H-T', ()=> {
+    console.log(Connected)
+}, e => console.error(e));
 
 
 app.get('/', (req, res) => {
