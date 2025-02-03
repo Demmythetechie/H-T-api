9f138afa103277f96bb9b08e25b6d3b9e40b1fb0
Demmythetechie <demmythetechie@gmail.com>
2025-02-03 20:52:08 +0100

    Connected my mongoose to my atlas cluster again, first try failed due to outdated use of mongoose

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const rules = cors();
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(e.json());
 
-mongoose.connect('mongodb+srv://demmythetechie:Alade3015@h-t.8p6yq.mongodb.net/?retryWrites=true&w=majority&appName=H-T', ()=> {
-    console.log(Connected)
-}, e => console.error(e));
+(async function () {
+    try {
+        await mongoose.connect('mongodb+srv://demmythetechie:Alade3015@h-t.8p6yq.mongodb.net/?retryWrites=true&w=majority&appName=H-T')
+        console.log('connected Successfully ✅')
+    } catch(error) {
+        console.error("MongoDB Connection Error ❌:", error);
+        process.exit(1);
+    }
+})();
 
 
 app.get('/', (req, res) => {
