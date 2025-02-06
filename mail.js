e2f5accd3f62bbde7cda146765ce612dd1729819
Demmythetechie <demmythetechie@gmail.com>
2025-02-07 00:57:22 +0100

    corrected policy problem with gmail

--- mail.js
+++ mail.js
@@ -7,7 +7,7 @@ async function mailer(receiver) {
         secure: true, // true for 465, false for 587
         auth: {
             user: "naim.okunade@gmail.com",
-            pass: "vhev ulzz aoog usvj",
+            pass: "vhevulzzaoogusvj",
         },
     });
 
