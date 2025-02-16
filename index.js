6f9de759094ca191d23ca93cf8525a81b341c796
Demmythetechie <demmythetechie@gmail.com>
2025-02-16 17:57:09 +0100

    Debugging test

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ app.get('/verify/:token', async (req, res) => {
         const verifying = jwt.verify(token, process.env.SECRET_KEY);
         await signUp.findOneAndUpdate({ Email: verifying.userEmail }, { $set: { verified: true } });
         const user = await signUp.findOne({Email: verifying.userEmail});
+        console.log('Running multiple times');
         verifiedReciept(verifying.userEmail, user.Firstname);
         res.render("confirmation", { 
             title: "Email Verified",
