3de7fa13d5dbc9b501162faf9b62865353fbf51e
Demmythetechie <demmythetechie@gmail.com>
2025-02-20 22:41:40 +0100

    added a logic that checks if the email has been sign up before or not

--- index.js
+++ index.js
@@ -110,6 +110,9 @@ app.post('/signin', async (req, res) => {
     try {
         const loginDetails = req.body;
         const log = await signUp.findOne({Email: loginDetails.email});
+        if (Object.keys(log).length === 0) {
+            return res.send(null);
+        }
         if (loginDetails.pswd === log.Password) {
             const access = jwt.sign({Email: loginDetails.email}, process.env.SECRET_KEY, { expiresIn: "24h" });
             res.cookie("token", access, {
