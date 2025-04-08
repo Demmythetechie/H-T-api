28b092f4c3712c4b4eb7d03bb720dc495cb521d6
Demmythetechie <demmythetechie@gmail.com>
2025-04-08 21:42:12 +0100

    Still on debugging the previous problem

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.post('/signin', async (req, res) => {
 app.get('/userInfo', async (req, res) => {
     try {
         const sentToken = req.header;
-        console.log(sentToken.Authorization);
+        console.log(sentToken);
         const loginTokenValidity = jwt.verify(sentToken['Authorization'], process.env.SECRET_KEY);
         const info = await signUp.findOne({Email: loginTokenValidity});
         res.json(info);
